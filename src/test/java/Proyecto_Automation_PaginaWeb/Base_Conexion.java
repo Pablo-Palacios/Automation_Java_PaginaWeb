37ecfbc6d55e71fed5e6b5e4437dead3235e22bf
Pablo Palacios <pablomartinpalacios27@gmail.com>
2023-10-16 23:17:44 -0300

    Archivo test

---
+++ src/test/java/Proyecto_Automation_PaginaWeb/Base_Conexion.java
@@ -0,0 +1,70 @@
+package Proyecto_Automation_PaginaWeb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class Base_Conexion {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+        ChromeOptions options = new ChromeOptions();
+
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+
+        options.addArguments("start-maximized");
+        options.addArguments("incognito");
+
+        options.setPageLoadTimeout(Duration.ofSeconds(60));
+
+        this.driver = new ChromeDriver(options);
+
+
+
+    }
+
+
+    public WebDriver getDriver(){
+        return this.driver;     //"https://opencart.abstracta.us/"
+    }
+
+
+    
+
+    @AfterMethod
+    public void tearDown(){
+        if(this.driver != null){
+            this.driver.quit();}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
